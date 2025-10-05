0bae82cd17dfad1a909ce2a70ae967b44ef9a2e1
Ankur Mehta <ankurmehta102@gmail.com>
2025-10-05 18:12:51 +0530

    feat(router): setup react-router-dom with initial route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { Routes, Route } from 'react-router-dom';
+import Signup from './pages/landing/Signup';
+import Login from './pages/landing/Login';
 
 function App() {
   return (
     <MantineProvider>
-      <div>Hello World</div>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </MantineProvider>
   );
 }
