3bb417d41b26a408f13eb61fc5a7442c807da185
Ankur Mehta <ankurmehta102@gmail.com>
2025-11-19 00:00:22 +0530

    feat(auth): implement logout on 401 unauthorized response

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,6 +1,20 @@
 import axios, { type AxiosInstance } from 'axios';
+import { logout } from './helper';
 
 export const api: AxiosInstance = axios.create({
   baseURL: 'http://localhost:5000',
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (err) => {
+    if (err.status === 401) {
+      logout();
+      return;
+    }
+    return Promise.reject(err);
+  },
+);
--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,3 +1,7 @@
 export const STORAGE_KEYS = {
   PROFILE_PICTURE_URL: 'profilePictureURL',
 } as const;
+
+export const logout = () => {
+  window.location.href = '/login';
+};
